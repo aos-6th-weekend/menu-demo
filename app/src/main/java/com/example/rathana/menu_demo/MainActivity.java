072ffcd8d191230fc9cac1955c65854c4b52addf
Rathana Voy <rathana@KSHRD-KPC-14s-Mac-mini.local>
2018-12-15 11:19:49 +0700

    context and pop menu

--- app/src/main/java/com/example/rathana/menu_demo/MainActivity.java
+++ app/src/main/java/com/example/rathana/menu_demo/MainActivity.java
@@ -2,18 +2,105 @@ package com.example.rathana.menu_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnContextMenu;
+    Button btnActionMode;
+    ActionMode mActionMode;
+
+    Button btnPopupMenu;
+
+
+    ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            getMenuInflater().inflate(R.menu.context_menu,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case  R.id.download:
+                    Toast.makeText(MainActivity.this, "Downloading ...", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.share:
+                    return true;
+                case R.id.getLink:
+                    return  true;
+                default: return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            mActionMode=null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnContextMenu=findViewById(R.id.btnContextMenu);
+        btnActionMode=findViewById(R.id.btnActionMode);
+        btnPopupMenu=findViewById(R.id.btnPopupMenu);
+
+        registerForContextMenu(btnContextMenu);
+
+        btnActionMode.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if(mActionMode!=null)
+                    return false;
+
+                mActionMode= startActionMode(actionModeCallback);
+                v.setSelected(true);
+                return true;
+            }
+        });
+
+        btnPopupMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu menu=new PopupMenu(MainActivity.this,v);
+                menu.inflate(R.menu.context_menu);
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch ( item.getItemId()){
+                            case R.id.download:
+                                Toast.makeText(MainActivity.this, "downloading ...", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.share:
+                                return true;
+                            case R.id.getLink:
+                                return  true;
+                            default: return  false;
+                        }
+                    }
+                });
+
+                menu.show();
+            }
+        });
     }
 
     @Override
@@ -58,6 +145,25 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
                 default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.context_menu,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.download:
+                Toast.makeText(this, "Downloading...", Toast.LENGTH_SHORT).show();
+                return  true;
+            case R.id.share:
+                return true;
+            case R.id.getLink:
+                return true;
+            default: return false;
         }
     }
 }
